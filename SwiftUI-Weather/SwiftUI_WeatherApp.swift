5fdce5401a3b6e3c25980760adcb2bdf9c2c2508
roshanadke <roshan.adke.ext@wsa.com>
2025-04-03 18:14:34 +0530

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Roshan Adke on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
